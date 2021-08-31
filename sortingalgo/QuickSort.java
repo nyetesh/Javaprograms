3def579f96765e5720f7c86f5cadfd3dfbab8859
nyetesh <51146473+nyetesh@users.noreply.github.com>
2021-08-31 21:09:46 +0545

    Update QuickSort.java

--- sortingalgo/QuickSort.java
+++ sortingalgo/QuickSort.java
@@ -51,10 +51,10 @@ public class QuickSort {
 
  /** A test method */
  public static void main(String[] args) {
- int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
- quickSort(list);
- for (int i = 0; i < list.length; i++)
- System.out.print(list[i] + " ");
+    int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+    quickSort(list);
+    for (int i = 0; i < list.length; i++)
+        System.out.print(list[i] + " ");
  }
  }
     
